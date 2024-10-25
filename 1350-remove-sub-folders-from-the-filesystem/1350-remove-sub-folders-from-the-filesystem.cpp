578470bdd12130bf3624d767c54a1ac30dd448f0
RishavJain21 <rishavjain2002@gmail.com>
2024-10-25 11:40:34 +0530

    Time: 75 ms (90.66%), Space: 85.1 MB (36.54%) - LeetHub

--- 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
+++ 1350-remove-sub-folders-from-the-filesystem/1350-remove-sub-folders-from-the-filesystem.cpp
@@ -1,7 +1,8 @@
 class TrieNode {
 public:
+    // Array to store pointers to child nodes; 26 letters + 1 for '/'
     TrieNode* children[27];
-    bool ie=false;
+    bool ie = false; // Indicates the end of a complete path (a valid folder)
 };
 
 class Trie {
@@ -12,38 +13,52 @@ public:
         root = new TrieNode();
     }
 
+    // Function to insert a folder path into the Trie
     bool insert(const string &word) {
         TrieNode* current = root;
-        for (int i=1;i<word.size();i++) {
-            if(word[i]=='/'){
-                if(!current->children[26]){
-                    current->children[26]=new TrieNode();
-                }else if(current->children[26]->ie==true){
-                    return 0;
+        for (int i = 1; i < word.size(); i++) {
+            if (word[i] == '/') {
+                // Move to or create a node representing the '/' character
+                if (!current->children[26]) {
+                    current->children[26] = new TrieNode();
+                } else if (current->children[26]->ie == true) {
+                    // If a complete folder path ends here, the current word is a sub-folder
+                    return false;
                 }
-                current=current->children[26];
-            }else{
-                if (!current->children[word[i]-'a']) {
-                    current->children[word[i]-'a'] = new TrieNode();
+                current = current->children[26];
+            } else {
+                // Move to or create a node for the current character
+                if (!current->children[word[i] - 'a']) {
+                    current->children[word[i] - 'a'] = new TrieNode();
                 }
-                current = current->children[word[i]-'a'];
+                current = current->children[word[i] - 'a'];
             }
         }
-        current->ie=true;
-        return 1;
+        // Mark the end of a valid folder path
+        current->ie = true;
+        return true;
     }
 };
+
 class Solution {
 public:
     vector<string> removeSubfolders(vector<string>& folder) {
-        Trie* trie=new Trie();
+        Trie* trie = new Trie(); // Create a Trie to store folder paths
         vector<string> ans;
+        
+        // Sort folders by length to ensure we process top-level folders first
         std::sort(folder.begin(), folder.end(), [](const std::string &a, const std::string &b) {
-        return a.length() < b.length();
-    });
-        for(auto i:folder){
-            if(trie->insert(i+'/')) ans.push_back(i);
+            return a.length() < b.length();
+        });
+
+        // Iterate through each folder
+        for (auto i : folder) {
+            // Try to insert the folder path into the Trie
+            if (trie->insert(i + '/')) {
+                ans.push_back(i); // If successful, it's not a sub-folder, add to the result
+            }
         }
+
         return ans;
     }
 };
