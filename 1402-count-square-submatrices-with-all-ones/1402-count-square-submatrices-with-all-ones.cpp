9850ed5742de821221250d160253d9375a39313f
RishavJain21 <rishavjain2002@gmail.com>
2024-10-27 21:54:38 +0530

    Time: 0 ms (100%), Space: 26.3 MB (92.75%) - LeetHub

--- 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
+++ 1402-count-square-submatrices-with-all-ones/1402-count-square-submatrices-with-all-ones.cpp
@@ -4,16 +4,16 @@ public:
         int n=matrix.size(),m=matrix[0].size();
         int ans=0;
         for(int i=0;i<n;i++){
-            ans+=matrix[i][0]==1;
+            ans=ans+(matrix[i][0]==1);
         }
         for(int i=1;i<m;i++){
-            ans+=matrix[0][i]==1;
+            ans=ans+(matrix[0][i]==1);
         }
         for(int i=1;i<n;i++){
             for(int j=1;j<m;j++){
                 if(matrix[i][j]==1){
                     matrix[i][j]=min({matrix[i-1][j],matrix[i][j-1],matrix[i-1][j-1]})+1;
-                    ans+=matrix[i][j];
+                    ans=ans+matrix[i][j];
                 }
                 
             }
