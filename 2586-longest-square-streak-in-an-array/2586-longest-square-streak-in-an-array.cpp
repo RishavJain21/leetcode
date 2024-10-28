f5b6f238b60a260b9fb1b1d649a793e13d7edb90
RishavJain21 <rishavjain2002@gmail.com>
2024-10-28 10:03:02 +0530

    Time: 58 ms (73.33%), Space: 127.9 MB (14.47%) - LeetHub

--- 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
+++ 2586-longest-square-streak-in-an-array/2586-longest-square-streak-in-an-array.cpp
@@ -1,15 +1,15 @@
 class Solution {
 public:
-    bool isRoot(int n){
-        return ((int)sqrt(n)*(int)sqrt(n))==n;
-    }
+    // bool isRoot(int n){
+    //     return ((int)sqrt(n)*(int)sqrt(n))==n;
+    // }
     int longestSquareStreak(vector<int>& nums) {
         vector<int> v(1e5+1,0);
         int ans=1;
         sort(nums.begin(),nums.end());
         for(auto i:nums){
             v[i]=1;
-            if(isRoot(i) && v[sqrt(i)]>0){
+            if(((int)sqrt(i)*(int)sqrt(i))==i && v[sqrt(i)]>0){
                 v[i]=v[sqrt(i)]+1;
                 ans=max(ans,v[i]);
             }
