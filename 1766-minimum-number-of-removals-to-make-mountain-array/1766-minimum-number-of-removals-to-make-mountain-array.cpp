c522d0a944a42cd34e0353cb55ea545ca5f5739e
RishavJain21 <rishavjain2002@gmail.com>
2024-10-30 09:54:38 +0530

    Time: 76 ms (25.76%), Space: 14.8 MB (67.26%) - LeetHub

--- 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
+++ 1766-minimum-number-of-removals-to-make-mountain-array/1766-minimum-number-of-removals-to-make-mountain-array.cpp
@@ -12,18 +12,16 @@ public:
             }
         }
 
+        int ans=n;
         for(int i=n-1;i>=0;i--){
             for(int j=n-1;j>i;j--){
                 if(nums[i]>nums[j]){
                     v2[i]=max(v2[i],v2[j]+1);
                 }
             }
-        }
-
-        int ans=n;
-        for(int i=0;i<n;i++){
             if(v1[i]!=1 && v2[i]!=1) ans=min(ans,n+1-v1[i]-v2[i]);
         }
+
         return ans;
     }
 };
