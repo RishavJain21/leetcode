77a157a971fb4d72edd6640191baa3bb60e3efc0
RishavJain21 <rishavjain2002@gmail.com>
2024-11-08 09:24:49 +0530

    Time: 7 ms (34.29%), Space: 95.4 MB (63.99%) - LeetHub

--- 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
+++ 1940-maximum-xor-for-each-query/1940-maximum-xor-for-each-query.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<int> getMaximumXor(vector<int>& nums, int k) {
         vector<int> ans(nums.size(),0);
-        int mask = (1 << k) - 1;
+        int mask = (1LL << k) - 1;
         int temp=0;
         for(int i=0;i<nums.size();i++){
             temp=temp^nums[i];
