2525b550ae314501ff29b7afb2b1d74d54f8f59f
RishavJain21 <rishavjain2002@gmail.com>
2025-02-10 07:29:30 +0530

    Time: 6 ms (40.46%), Space: 17.2 MB (10.75%) - LeetHub

--- 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
+++ 3018-make-string-a-subsequence-using-cyclic-increments/3018-make-string-a-subsequence-using-cyclic-increments.cpp
@@ -1,13 +1,16 @@
 class Solution {
 public:
     bool canMakeSubsequence(string str1, string str2) {
+        if(str2.length() > str1.length()){
+            return false;
+        }
         int i=0,j=0;
-        while(i<str1.size() && j<str2.size()){
+        while(i<str1.length() && j<str2.length()){
             if(str1[i]==str2[j] || ((str1[i]-'a')+1)%26==(str2[j]-'a')){
                 j++;
             }
             i++;
         }
-        return j==str2.size();
+        return j==str2.length();
     }
 };
