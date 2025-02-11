a18a50b3d443388b4683459cdccb46256be986fc
RishavJain21 <rishavjain2002@gmail.com>
2025-02-11 09:42:55 +0530

    Time: 0 ms (100%), Space: 19.8 MB (60.63%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,34 +1,31 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        vector<int> ans;
-        int n=nums.size();
-        int cnt1=0,cnt2=0;
-        int e1=INT_MIN,e2=INT_MIN;
-
-        for(int i=0;i<n;i++){
-            if(cnt1==0 && nums[i]!=e2){
-                e1=nums[i];
-                cnt1++;
-            }else if(cnt2==0 && nums[i]!=e1){
-                e2=nums[i];
-                cnt2++;
-            }else if(nums[i]==e1) cnt1++;
-            else if(nums[i]==e2) cnt2++;
+        int a=INT_MIN,b=INT_MIN;
+        int c1=0,c2=0;
+        for(int i=0;i<nums.size();i++){
+            if(c1==0 && nums[i]!=b){
+                c1++;
+                a=nums[i];
+            }else if(c2==0 && nums[i]!=a){
+                c2++;
+                b=nums[i];
+            }else if(nums[i]==a)c1++;
+            else if(nums[i]==b)c2++;
             else{
-                cnt1--;cnt2--;
+                c1--;
+                c2--;
             }
         }
-        cnt1=0;cnt2=0;
-
-        for(int i=0;i<n;i++){
-            if(nums[i]==e1) cnt1++;
-            else if(nums[i]==e2) cnt2++;
+        int c=int(nums.size()/3)+1;
+        c1=0,c2=0;
+        vector<int> ans;
+        for(int i=0;i<nums.size();i++){
+            if(nums[i]==a)c1++;
+            if(nums[i]==b)c2++;
         }
-
-        int a=(int)n/3 + 1;
-        if(cnt1>=a) ans.push_back(e1);
-        if(cnt2>=a) ans.push_back(e2);
+        if(c1>=c) ans.push_back(a);
+        if(c2>=c) ans.push_back(b);
 
         return ans;
     }
